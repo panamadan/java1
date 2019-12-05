cefb8300333cf5ace3fd6671a31729275f44de10
Juan De La Guardia <delaguardia@outlook.com>
2019-12-04 19:06:17 -0500

    button

--- java.js
+++ java.js
@@ -1,9 +1,36 @@
 function generatePassword() {
     var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        charset = "",
+        uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        lower = "abcdefghijklmnopqrstuvwxyz",
+        numeric = "0123456789",
+        special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '""',
         retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
+        n = charset.length;
+        var upperCaseprompt = prompt("Do you want upper case latters in your password?");
+        if (upperCaseprompt === true) {
+           charset += uppercase 
+        }
+
+
+// repeate for the 3 others
+
+
+// add innertext for id=password for inside the box to generate the passwor
+    for (var i = 0 ; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
     return retVal;
+
+
 }
+
+var generateButton = document.getElementById("generate");
+generateButton.addEventListener("click", function () {
+    
+    console.log(generatePassword());
+})
+
+
+
+
